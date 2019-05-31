59fda4ee93c74e8dedf42003322df61126cd2d61
SeluneXscd <1353977352@qq.com>
2019-05-31 22:18:45 +0800

    fix: finish UserDaoTest

--- src/com/selune/mybatis/test/UserDaoTest.java
+++ src/com/selune/mybatis/test/UserDaoTest.java
@@ -35,4 +35,27 @@ public class UserDaoTest {
         Assert.assertEquals("张三", user.getUsername());
         System.out.println(user);
     }
+
+    @Test
+    public void insertUser() throws Exception {
+        UserDao userDao = new UserDaoImpl(sqlSessionFactory);
+        User user = new User();
+        user.setUsername("wang5");
+        userDao.insertUser(user);
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+        UserDao userDao = new UserDaoImpl(sqlSessionFactory);
+        User user = new User();
+        user.setId(15);
+        user.setUsername("li4");
+        userDao.updateUser(user);
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        UserDao userDao = new UserDaoImpl(sqlSessionFactory);
+        userDao.deleteUser(15);
+    }
 }
