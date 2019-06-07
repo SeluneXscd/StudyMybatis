26d84d7df3a9bdfe877930ea69e5da00c391def4
Selune <1353977352@qq.com>
2019-06-07 22:24:49 +0800

    fix: update table.sql

--- config/sql/createTable.sql
+++ config/sql/createTable.sql
@@ -1,5 +1,18 @@
 use test;
 
+CREATE TABLE `orders_`
+(
+    `order_id`   int      NOT NULL COMMENT '订单号',
+    `user_id`    int      NOT NULL COMMENT '用户id',
+    `createTime` datetime NOT NULL COMMENT '下单时间',
+    `note`       varchar(100) DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`order_id`),
+    KEY `FK_orders_1` (`user_id`),
+    CONSTRAINT `FK_orders_id` FOREIGN KEY (`user_id`) REFERENCES `user_` (`id`)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB
+  DEFAULT CHARSET = UTF8;
+
 CREATE TABLE `user_`
 (
     `id`       int         NOT NULL AUTO_INCREMENT COMMENT '用户id',
@@ -10,20 +23,6 @@ CREATE TABLE `user_`
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10001
-  DEFAULT CHARSET = UTF8;
-
-CREATE TABLE `orders_`
-(
-    `id`         int      NOT NULL AUTO_INCREMENT,
-    `user_id`    int      NOT NULL COMMENT '用户id',
-    `order_id`   int      NOT NULL COMMENT '订单号',
-    `createTime` datetime NOT NULL COMMENT '下单时间',
-    `note`       varchar(100) DEFAULT NULL COMMENT '备注',
-    PRIMARY KEY (`id`),
-    KEY `FK_orders_1` (`user_id`),
-    CONSTRAINT `FK_orders_id` FOREIGN KEY (`user_id`) REFERENCES `user_` (`id`)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB
   DEFAULT CHARSET = UTF8;
 
 CREATE TABLE `items_`
@@ -43,18 +42,18 @@ CREATE TABLE `items_`
 CREATE TABLE `orderdetail_`
 (
     `id`            int NOT NULL AUTO_INCREMENT,
-    `order_id`      int NOT NULL COMMENT '订单id',
-    `item_id`       int NOT NULL COMMENT '商品id',
-    `item_quantity` int NOT NULL COMMENT '商品数量',
+    `orders_id`      int NOT NULL COMMENT '订单id',
+    `items_id`       int NOT NULL COMMENT '商品id',
+    `items_quantity` int NOT NULL COMMENT '商品数量',
     PRIMARY KEY (`id`),
-    KEY `FK_orderdetail_1` (`order_id`),
-    KEY `FK_orderdetail_2` (`item_id`),
-    CONSTRAINT `FK_orderdetail_1` FOREIGN KEY (`order_id`) REFERENCES `orders_` (`id`) ON DELETE
+    KEY `FK_orderdetail_1` (`orders_id`),
+    KEY `FK_orderdetail_2` (`items_id`),
+    CONSTRAINT `FK_orderdetail_1` FOREIGN KEY (`orders_id`) REFERENCES `orders_` (`order_id`) ON
+        DELETE
         NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `FK_orderdetail_2` FOREIGN KEY (`item_id`) REFERENCES `items_` (`item_id`) ON
+    CONSTRAINT `FK_orderdetail_2` FOREIGN KEY (`items_id`) REFERENCES `items_` (`item_id`) ON
         DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 1000001
   DEFAULT CHARSET = UTF8;
 
 
